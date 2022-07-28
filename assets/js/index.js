f1b506d096814f9c3d9df3a199283ac95e56a3d9
killian <72389975+buldOo@users.noreply.github.com>
2022-07-28 15:08:06 +0200

    Add the about part
    - Add the about part
    - Add the sidebar navigation system

---
+++ assets/js/index.js
@@ -0,0 +1,8 @@
+// Get all the sidebar Li
+var liList = Array.prototype.slice.call(document.getElementsByClassName('sidebar-li'));
+
+// Add a onclick function to change the color of the <Li> we clicked on and reset the other
+liList.forEach(element => element.addEventListener('click', function handleClick() {
+    liList.forEach(element => element.classList.remove("active"));
+    element.classList.add("active");
+}));
